a7a1bb382e388de68cfb70c78ecf81c63da0aacc
anekil <78557681+anekil@users.noreply.github.com>
2023-10-24 15:14:05 +0200

    Added navigation dependencies

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>EventTu!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>EventTu!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
   );
 }
 
